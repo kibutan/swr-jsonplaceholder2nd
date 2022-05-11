7c71c66b0b0562565a4fbed1a09bab652258a4e7
kibutan <s1170230@gmail.com>
2022-05-11 14:29:51 +0900

    Infinity looping lorem box

--- src/components/MyBox.tsx
+++ src/components/MyBox.tsx
@@ -1,5 +1,17 @@
+import { Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
 function MyBox(): JSX.Element {
-  return <div>MyBox</div>;
+  return (
+    <Box
+      as={motion.div}
+      w="40px"
+      h="40px"
+      bgColor="whiteAlpha.100"
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.8 }}
+    />
+  );
 }
 
 export default MyBox;
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,7 +1,10 @@
-import Post from "./Post";
+// import Post from "./Post";
 import { useGetPosts } from "../useRequest";
 import { Box, HStack, Stack } from "@chakra-ui/react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 function Posts() {
+  const [count, setCount] = useState(1);
   const { posts, error } = useGetPosts("/posts");
   if (error) return <div>failed to load in Posts.tsx</div>;
   if (!posts) return <div>loading Now...</div>;
@@ -10,28 +13,42 @@ function Posts() {
       {/* {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))} */}
-      <Stack w="70vw">
-        {posts.map((post) => (
-          <Box
-            _hover={{ bgColor: "gray.500" }}
-            _active={{ bgColor: "gray.700" }}
-            bgColor="gray.600"
-            rounded="20px"
-          >
-            {/* <HStack m={3} p={2}> */}
-            <Box m={3} fontSize="medium" color="gray.200">
-              {post.id}
-            </Box>
-            <Box m={3} fontWeight="black" fontSize="4xl" color="gray.50">
-              {post.title}
-            </Box>
-            {/* </HStack> */}
+      <Stack
+        w="70vw"
+        onClick={() => {
+          setCount((prevState) => (prevState % posts.length) + 1);
+          console.log(count);
+        }}
+      >
+        {posts.map((post) =>
+          post.id === count ? (
+            <Box
+              as={motion.div}
+              // whileHover={{ scale: 1.0 }}
+              whileTap={{ scale: 0.98 }}
+              key={post.id}
+              _hover={{ bgColor: "gray.500" }}
+              _active={{ bgColor: "gray.700" }}
+              bgColor="gray.600"
+              rounded="20px"
+            >
+              {/* <HStack m={3} p={2}> */}
+              <Box m={3} fontSize="medium" color="gray.200">
+                {post.id}
+              </Box>
+              <Box m={3} fontWeight="black" fontSize="4xl" color="gray.50">
+                {post.title}
+              </Box>
+              {/* </HStack> */}
 
-            <Box m={3} fontSize="lg" color="gray.400">
-              {post.body}
+              <Box m={3} fontSize="lg" color="gray.400">
+                {post.body}
+              </Box>
             </Box>
-          </Box>
-        ))}
+          ) : (
+            ""
+          )
+        )}
       </Stack>
     </div>
   );
